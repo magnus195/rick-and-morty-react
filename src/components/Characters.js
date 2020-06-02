e9d319b6ec9294ed1c1f3ac2a99169a689f7765f
Magnus <magnus.e@outlook.com>
2020-06-02 13:49:58 +0200

    Bonus

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
 import Character from "../components/Character";
-import {Row} from 'reactstrap'
+import {Row, Input, InputGroup, InputGroupAddon, InputGroupText, Col, Button} from 'reactstrap'
 
-async function getData(url) {
+async function getCharacters(url) {
     const res = await fetch(url, {
         method: 'GET',
         headers: {
@@ -16,22 +16,63 @@ const Characters = () => {
     // Try to think through what state you'll need for this app before starting. Then build out
     // the state properties here.
     const [characters, setCharacters] = useState({})
+    const [search, setSearch] = useState("")
+    const [page, setPage] = useState(1)
     // Fetch characters from the API in an effect hook. Remember, anytime you have a
     // side effect in a component, you want to think about which state and/or props it should
     // sync up with, if any.
+    function searchFieldChange(e) {
+        setSearch(e.target.value)
+    }
+    function pageIncrease() {
+        setPage(page + 1)
+    }
+    function pageDecrease() {
+        setPage(page - 1)
+    }
+
     useEffect(() => {
-        getData('https://rickandmortyapi.com/api/character/').then(data => {
+        getCharacters('https://rickandmortyapi.com/api/character/?name=' + search + '&page=' + page).then(data => {
             console.log(JSON.stringify(data))
-            let characterData = data.results.map(character => character)
+            let characterData
+            try {
+                characterData = data.results.map(character => character)
+            } catch {
+                characterData = {}
+                console.log("error")
+            }
             setCharacters(characterData)
         })
-    }, [])
+    }, [search, page])
 
     return (<div>
+        <Row xs={"1"} md={"2"} lg={"2"} className={"justify-content-center"}>
+            <Col>
+            <InputGroup>
+                <InputGroupAddon addonType={"prepend"}>
+                    <InputGroupText>Search</InputGroupText>
+                </InputGroupAddon>
+                <Input type={"text"} name={"search"} id={"searchField"} onChange={searchFieldChange}/>
+            </InputGroup>
+            </Col>
+        </Row>
         <Row xs={"1"} sm={"2"} md={"4"} lg={"5"}>
             {Object.keys(characters).map(i => {
                 return <Character character={characters[i]}/>
             })}
+        </Row>
+        <Row xs={"1"} md={"2"} lg={"2"} className={"justify-content-center"}>
+            <Col>
+                <InputGroup>
+                    <InputGroupAddon addonType={"prepend"}>
+                        <Button onClick={pageDecrease}>Previous</Button>
+                    </InputGroupAddon>
+                    <InputGroupText>{page}</InputGroupText>
+                    <InputGroupAddon addonType={"append"}>
+                        <Button onClick={pageIncrease}>Next</Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </Col>
         </Row>
     </div>);
 };
